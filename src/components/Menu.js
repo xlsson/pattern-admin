2dd8e3e2e483ecfe476f767da3addeda040ad446
U-RICHARDASUS\Richard <richard.axelsson@gmail.com>
2021-11-24 01:35:14 +0100

    Added more menu options

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
-function Menu() {
+import ChangeCity from './ChangeCity';
+
+function Menu(props) {
     function handleClick(choice) {
-        console.log(choice);
+        props.menuChoice(choice);
+    }
+
+    function handleCityChange(cityId) {
+        console.log(cityId);
     }
 
     return (
-        <ul className="menu-wrapper">
-            <li onClick={handleClick("city")}>Byt stad</li>
-            <li onClick={handleClick("users")}>Kunder</li>
-            <li onClick={handleClick("charge")}>Laddningsstationer</li>
-            <li onClick={handleClick("parking")}>Parkeringszoner</li>
-            <li onClick={handleClick("map")}>Karta</li>
-        </ul>
+        <div className="header-wrapper">
+            <ChangeCity cityChoice={handleCityChange} />
+            <ul className="menu-wrapper">
+                <li onClick={() => handleClick("users")}>Kunder</li>
+                <li onClick={() => handleClick("bikes")}>Cyklar</li>
+                <li onClick={() => handleClick("chargingStations")}>Laddningsstationer</li>
+                <li onClick={() => handleClick("parkingStations")}>Parkeringszoner</li>
+                <li onClick={() => handleClick("map")}>Karta</li>
+            </ul>
+        </div>
     );
 }
 
