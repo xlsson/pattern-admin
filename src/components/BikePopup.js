187fbac8645b779d0f3019b097a854284a1f8748
U-RICHARDASUS\Richard <richard.axelsson@gmail.com>
2021-12-25 22:59:48 +0100

    Split component up into two

--- src/components/BikePopup.js
+++ src/components/BikePopup.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from "prop-types";
 import BikeEndMaintenance from './BikeEndMaintenance';
+import BikePopupMoveForm from './BikePopupMoveForm';
 
 BikePopup.propTypes = {
     api: PropTypes.object,
@@ -15,56 +16,21 @@ function BikePopup(props) {
     const bike = props.bike;
     const chargeStations = props.cities[bike.city_id].charge_stations;
 
-    const [selectedId, setSelectedId] = useState("");
-    const [maintenance, setMaintenance] = useState(false);
-
-    useEffect(() => { setSelectedId(0); }, [props]);
-
-    async function moveBike() {
-        const selectedStation = chargeStations[selectedId];
-        const data = await props.api.moveBike(bike._id, selectedStation);
-        console.log(data);
-        if (maintenance) {
-            await props.api.orderMaintenance(bike._id, true);
-            setMaintenance(false);
-        }
-        props.redrawBikes();
-        props.mapInstance.closePopup();
-    }
-
-    function stationSelection(event) {
-        setSelectedId(event.target.value);
-    }
-
-    function renderEndMaintenance(bike) {
+    function renderEndMaintenance() {
         return ( <BikeEndMaintenance
                     api={props.api}
                     redrawBikes={props.redrawBikes}
                     bike={bike} /> )
     }
 
-    function drawMoveBikeForm() {
-        return (
-            <div className="bike-popup-form">
-                <strong>Boka hämtning till:</strong>
-                <select onBlur={stationSelection}>
-                    {chargeStations.map((station, i) => (
-                        <option key={i} value={i}>
-                            {station.name}
-                        </option>
-                    ))}
-                </select>
-                <div>
-                    <label htmlFor="maintenance">Med underhåll</label>
-                    <input
-                        name="maintenance"
-                        type="checkbox"
-                        onClick={() => setMaintenance(!maintenance)}>
-                    </input>
-                </div>
-                <button type="button" onClick={moveBike}>Boka</button>
-            </div>
-        )
+    function renderBikePopupMoveForm() {
+        return ( <BikePopupMoveForm
+                    api={props.api}
+                    cities={props.cities}
+                    redrawBikes={props.redrawBikes}
+                    mapInstance={props.mapInstance}
+                    bike={bike}
+                    chargeStations={chargeStations} /> )
     }
 
     return (
@@ -77,8 +43,8 @@ function BikePopup(props) {
             <div>Status: {bike.bike_status}</div>
             <div>Batterinivå: {bike.battery_status}</div>
             <div>{(bike.mainenance) && "Genomgår underhåll"}</div>
-            {(bike.bike_status === "available") && drawMoveBikeForm(bike)}
-            {(bike.maintenance && (bike.battery_status === 100)) && renderEndMaintenance(bike)}
+            {(bike.bike_status === "available") && renderBikePopupMoveForm()}
+            {(bike.maintenance && (bike.battery_status === 100)) && renderEndMaintenance()}
         </div>
     );
 }
---
+++ src/components/BikePopupMoveForm.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from "prop-types";
+
+BikePopupMoveForm.propTypes = {
+    api: PropTypes.object,
+    cities: PropTypes.object,
+    redrawBikes: PropTypes.func,
+    mapInstance: PropTypes.object,
+    bike: PropTypes.object
+};
+
+function BikePopupMoveForm(props) {
+    const bike = props.bike;
+
+    const [selectedId, setSelectedId] = useState("");
+    const [maintenance, setMaintenance] = useState(false);
+
+    useEffect(() => { setSelectedId(0); }, [props]);
+
+    async function moveBike() {
+        const selectedStation = props.chargeStations[selectedId];
+        const data = await props.api.moveBike(bike._id, selectedStation);
+        console.log(data);
+        if (maintenance) {
+            await props.api.orderMaintenance(bike._id, true);
+            setMaintenance(false);
+        }
+        props.redrawBikes();
+        props.mapInstance.closePopup();
+    }
+
+    function stationSelection(event) {
+        setSelectedId(event.target.value);
+    }
+
+    return (
+        <div className="bike-popup-form">
+            <strong>Boka hämtning till:</strong>
+            <select onBlur={stationSelection}>
+                {props.chargeStations.map((station, i) => (
+                    <option key={i} value={i}>
+                        {station.name}
+                    </option>
+                ))}
+            </select>
+            <div>
+                <label htmlFor="maintenance">Med underhåll</label>
+                <input
+                    name="maintenance"
+                    type="checkbox"
+                    onClick={() => setMaintenance(!maintenance)}>
+                </input>
+            </div>
+            <button type="button" onClick={moveBike}>Boka</button>
+        </div>
+    );
+}
+
+export default BikePopupMoveForm;
