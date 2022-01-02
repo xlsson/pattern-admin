9be9debba2f5dc15e5a16e6c65fb59d77120c6fa
U-RICHARDASUS\Richard <richard.axelsson@gmail.com>
2022-01-02 14:19:19 +0100

    Added function to count bikes for each station

--- src/components/StationsTable.js
+++ src/components/StationsTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from "prop-types";
 
 StationsTable.propTypes = {
+    api: PropTypes.object,
     switchView: PropTypes.func,
     type: PropTypes.string,
     currentCity: PropTypes.object,
@@ -13,19 +14,50 @@ function StationsTable(props) {
 
     let title = (props.type === "charge") ? "Laddningsstationer" : "Parkeringsstationer";
 
-    useEffect(() => {
-        let _stations = createStationsArray();
+    useEffect(() => { createData(); }, [props]);
+
+    async function createData() {
+        const _bikes = await getBikes();
+        const _bikesPerStation = countBikes(_bikes);
+        const _stations = createStationsArray(_bikesPerStation);
         setStations(_stations);
-    }, [props]);
+    }
+
+    async function getBikes() {
+        const data = await props.api.getBikes(props.currentCity._id);
+        return data.bikes;
+    }
+
+    function countBikes(_bikes) {
+        const _bikesPerStation = {};
+        Object.keys(props.cities).forEach((key) => {
+            if (key !== "all") {
+                props.cities[key][`${props.type}_stations`].forEach((station) => {
+                    _bikesPerStation[station._id] = 0;
+                });
+            }
+        });
+
+        let stationId;
+        _bikes.forEach((_bike) => {
+            stationId = _bike[`${props.type}_id`];
+            if (stationId) { _bikesPerStation[stationId] += 1; }
+        });
+
+        return _bikesPerStation;
+    }
 
     // Adds city_id to each station and, if "all" is selected, creates one
     // array for all stations
-    function createStationsArray() {
+    function createStationsArray(_bikesPerStation) {
         let _stations = [];
 
         if (props.currentCity._id !== "all") {
             _stations = props.currentCity[`${props.type}_stations`];
-            _stations.forEach((station) => { station.city_id = props.currentCity._id;  });
+            _stations.forEach((station) => {
+                station.city_id = props.currentCity._id;
+                station.nrOfBikes = _bikesPerStation[station._id];
+            });
             return _stations;
         }
 
@@ -33,6 +65,7 @@ function StationsTable(props) {
             if (key !== "all") {
                 props.cities[key][`${props.type}_stations`].forEach((station) => {
                     station.city_id = key;
+                    station.nrOfBikes = _bikesPerStation[station._id];
                 });
                 _stations = _stations.concat(props.cities[key][`${props.type}_stations`]);
             }
@@ -54,7 +87,7 @@ function StationsTable(props) {
                     <tr>
                         <th>Stad</th>
                         <th>Stationsnamn</th>
-                        <th>Koordinater</th>
+                        <th className="text-align-center">Antal cyklar</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -65,11 +98,15 @@ function StationsTable(props) {
                         onClick={() => handleClick(i)}>
                         <>
                         <td>{props.cities[station.city_id].name}</td>
-                        <td>{station.name}</td>
                         <td>
-                            {station.coordinates.northwest.lat}, {station.coordinates.northwest.long},
-                            {station.coordinates.southeast.lat}, {station.coordinates.southeast.long}
+                        <div className="icon-and-label-wrapper">
+                            <span className="material-icons">
+                            {(props.type === "charge") ? "battery_charging_full" : "local_parking"}
+                            </span>
+                            <div>{station.name}</div>
+                        </div>
                         </td>
+                        <td className="text-align-center">{station.nrOfBikes}</td>
                         </>
                     </tr>
                 ))}
