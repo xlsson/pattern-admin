df0bc2d107f6e5e325ae0aa6c7eeaba8be77029e
U-RICHARDASUS\Richard <richard.axelsson@gmail.com>
2022-01-08 01:00:57 +0100

    Functions moved out of components

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -1,5 +1,3 @@
-import api from './api.js';
-
 const utils = {
     mapInstance: null,
     currentInterval: "",
@@ -8,6 +6,31 @@ const utils = {
         clearInterval(utils.currentInterval);
         utils.autoFetch = false;
     },
+    // Returns an array with coordinates for all city limits, for Leaflet map
+    createCityLimits: function (city, cities) {
+        const limitsArray = [];
+
+        if (city._id !== "all") {
+            limitsArray.push(city.coordinates);
+            return limitsArray;
+        }
+
+        Object.keys(cities).forEach((c) => {
+            if (c !== "all") { limitsArray.push(cities[c].coordinates); }
+        });
+
+        return limitsArray;
+    },
+    // Returns stationname based on station type and station id
+    getStationName: function(type, bike, cities) {
+        const stationType = `${type}_stations`;
+        const stations = cities[bike.city_id][stationType];
+        let stationName;
+        stations.forEach((station) => {
+            if (station._id === bike[`${type}_id`]) { stationName = station.name; }
+        });
+        return stationName;
+    },
     // Returns an array of all stations for the selected city id, with
     // city_id and city_name attribute added to each station
     createStationsArray: function(type, currentCity, cities) {
@@ -15,7 +38,11 @@ const utils = {
 
         if (currentCity._id !== "all") {
             stations = currentCity[`${type}_stations`];
-            stations.forEach((station) => { station.city_id = currentCity._id; });
+            stations.forEach((station) => {
+                station.city_id = currentCity._id;
+                station.city_name = currentCity.name;
+            });
+
             return stations;
         }
 
@@ -33,9 +60,7 @@ const utils = {
     },
     // Returns an object with the city's station ids as keys, and
     // the number of bikes at each station as their value
-    countBikes: async function(stations, type, cityId) {
-        const data = await api.getBikes(cityId);
-        const bikes = data.bikes;
+    countBikes: function(stations, type, bikes) {
         const result = {};
 
         stations.forEach((station) => { result[station._id] = 0; });
@@ -49,16 +74,19 @@ const utils = {
         return result;
     },
     getDateTimeString: function(mongoDate) {
-        const startTime = new Date(mongoDate);
+        const dateTime = new Date(mongoDate);
+        const diff = (dateTime.getTimezoneOffset());
 
-        const year = startTime.getFullYear();
-        const month = utils.addZero(startTime.getMonth());
-        const date = utils.addZero(startTime.getDate());
-        const hours = utils.addZero(startTime.getHours());
-        const minutes = utils.addZero(startTime.getMinutes());
+        dateTime.setMinutes(dateTime.getMinutes() + diff);
 
-        const time = `${year}-${month}-${date}, ${hours}:${minutes}`;
-        return time;
+        const year = dateTime.getFullYear();
+        const month = utils.addZero((dateTime.getMonth() + 1));
+        const date = utils.addZero(dateTime.getDate());
+        const hours = utils.addZero(dateTime.getHours());
+        const minutes = utils.addZero(dateTime.getMinutes());
+
+        const asString = `${year}-${month}-${date}, ${hours}:${minutes}`;
+        return asString;
     },
     addZero: function(number) {
         number = number.toString();
@@ -94,7 +122,8 @@ const utils = {
         };
 
         if (Object.prototype.hasOwnProperty.call(data, "message")) {
-            return { text: data.message, error: false };
+            const message = `Servermeddelande: ${data.message}`;
+            return { text: message, error: false };
         }
 
         const isError = Object.prototype.hasOwnProperty.call(data, "error");
